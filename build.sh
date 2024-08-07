84595689eb58c1ad855a649ad3a743ce290a4cb7
alexandre <alexandren49@gmail.com>
2024-08-07 18:45:33 -0300

    comments

--- build.sh
+++ build.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+#compiles libcpp.cpp file -fpic (position independent code, PIC)
 g++ -c -fpic libcpp.cpp -o libcpp.o
+
+#shared lib
 g++ -shared -Wl,-soname,libcpp.so.1 -o libcpp.so.1.0 libcpp.o
+
+#compile the file main.c and link the library
 gcc -o main main.c -L. -lcpp -Wl,-rpath=.
